ca6407efdcfcd04f677ace4312cca928b7e64dd0
CamilaPecho <camila_pfccta@hotmail.com.ar>
2021-11-23 11:20:33 -0300

    agregando cosas para el heroku

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 app.use(express.static('public'));
 
 //(etiqueta lo pisa la clase y la clase la pisa el id) 
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando estilos');
+app.listen (process.env.PORT || 3000, ()=>{
+    console.log('Servidor funcionando');
 });
 
 app.get('/', (req,res)=>{
